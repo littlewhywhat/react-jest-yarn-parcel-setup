deb5e0ec8e972b6a8f0a5b8a66328e13574e5550
littlewhywhat <littlewhywhat@gmail.com>
2019-11-10 13:35:48 +0100

    Fix

--- features/support/e2e.ts
+++ features/support/e2e.ts
@@ -3,5 +3,5 @@ import { config } from "./config";
 
 const cwd = process.cwd();
 const parcelPath = `${cwd}/node_modules/.bin/parcel`;
-const index = `${process.cwd()}/src/index.html`;
+const index = `${cwd}/src/index.html`;
 spawn(parcelPath, ["serve", index, "--port", `${config.port}`]);
